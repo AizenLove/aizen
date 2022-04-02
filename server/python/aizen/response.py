a5172c87c3697daa608c0634aa5d7a4a473dca06
d-kimsuon <d-kimuson@gmail.com>
2022-04-03 02:13:25 +0900

    chore(openapi): フロントで使いたいからスキーマ渡すよ

---
+++ server/python/aizen/response.py
@@ -0,0 +1,11 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class QueryBetaResponse(BaseModel):
+    id: str
+    title: str
+    base_url: str
+    describe: str
+    image: str
+    tags: List[str]
