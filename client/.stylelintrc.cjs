96522a1866cec9a2f509c67079cb05e8af730946
d-kimsuon <d-kimuson@gmail.com>
2022-04-02 23:17:09 +0900

    chore(*): 最低限 linter と型チェック回すよ

---
+++ client/.stylelintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    "stylelint-config-recommended-scss",
+    "stylelint-config-standard-scss",
+    "stylelint-config-prettier",
+  ],
+  plugins: ["stylelint-scss"],
+};
