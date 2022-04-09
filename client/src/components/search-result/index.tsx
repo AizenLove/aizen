04051306ed33eb9d114a2a116da787397bf38365
d-kimsuon <d-kimuson@gmail.com>
2022-04-10 02:18:44 +0900

    feat(search-result): ハードコーディングだけどサンプル動画みれるようにしちゃった

--- client/src/components/search-result/index.tsx
+++ client/src/components/search-result/index.tsx
@@ -6,10 +6,23 @@ type SearchResultProps = {
 };
 
 export const SearchResult: React.VFC<SearchResultProps> = ({ data }) => {
+  // 一時的ににハードコーディングするよ
+  const cid = `nygw0006`;
   return (
     <div className={styles.resultContainer}>
-      <img src={data.image} />
       <h2>{data.title}</h2>
+      <img src={data.image} />
+      <video
+        style={{
+          width: "90%",
+        }}
+        controls={true}
+        data-binding="play"
+        data-service="digital"
+        data-mode="detail"
+        autoPlay={false}
+        src={`https://cc3001.dmm.co.jp/litevideo/freepv/n/nyg/${cid}/${cid}_mhb_w.mp4`}
+      ></video>
       <a href={data.base_url}>FANZA で確認する</a>
       <div>{data.describe}</div>
     </div>
